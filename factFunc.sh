fd0ea647423ead3f1dde14582ebe78968d09f715
Ashutosh Pandey <baralbisnupriya7@gmail.com>
2022-02-11 12:33:11 +0530

    factorial program added

---
+++ factFunc.sh
@@ -0,0 +1,17 @@
+echo "Enter a number"
+read num
+
+fact=1
+
+function factorial()
+{
+
+fact=1
+
+for((i=2;i<=num;i++))
+do
+  fact=$((fact * i))
+done
+echo "Factoril of a given number :" $fact
+}
+factorial
